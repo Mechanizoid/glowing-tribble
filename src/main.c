6ec79b20dd43b699e37762a6b4eb4e8a9de47f1a
Christopher Orion Phoenix <orionphoenix93@gmail.com>
2023-03-29 22:42:10 -0700

    Draw three triangles using glDrawArrays()

--- src/main.c
+++ src/main.c
@@ -61,27 +61,38 @@ int main(void)
 	/* === Store Elements in Buffers === */
 	
 	/* vertex array */
+	/* modify this too add TWO triangles */
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f, 0.5f, 0.0f,  // top left
-		0.5f, 0.5f, 0.0f,   // top right
-		0.5f, -0.5f, 0.0f   // bottom right
-	};
+		// first triangle
+		-0.75f, 0.75f, 0.0f,   // bottom left
+		-0.5f, 0.25f, 0.0f,     // top
+		-0.25f, 0.75f, 0.0f,   // bottom right
+		// second triangle
+		0.25f, 0.75f, 0.0f,    // bottom left
+		0.5f, 0.25f, 0.0f,      // top 
+		0.75f, 0.75f, 0.0f,    // bottom right
+		// third triangle
+		-0.25f, -0.6f, 0.0f,     // bottom left
+		0.0f, -0.1f, 0.0f,      // top
+		0.25f, -0.6f, 0.0f,      // bottom right
+	}; 
 
-	unsigned int indices[] = {
-		0, 1, 2,           // first triangle
-		0, 3, 2
-	};
+	/* unsigned int indices[] = { */
+	/* 	0, 1, 2,           // first triangle */
+	/* 	3, 4, 5, */
+	/* 	6, 7, 8, */
+	/* }; */
 
 	/* Create vertex array object to hold our vertex buffer and index
 	 * buffer objects, then initialize them and store vertices and
 	 * indices in their respective objects.
 	 */
-	unsigned int vao, vbo, ebo;
+	unsigned int vao, vbo;
+	//unsigned int ebo;
 
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
-	glGenBuffers(1, &ebo);
+	//glGenBuffers(1, &ebo);
 
 	glBindVertexArray(vao);
 	
@@ -89,9 +100,9 @@ int main(void)
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
 		     GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-		     GL_STATIC_DRAW);
+	/* glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo); */
+	/* glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, */
+	/* 	     GL_STATIC_DRAW); */
 	
 	
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
@@ -112,7 +123,8 @@ int main(void)
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glUseProgram(shader_prog);
 		glBindVertexArray(vao);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 9);
+		//glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		/* swap buffers */
